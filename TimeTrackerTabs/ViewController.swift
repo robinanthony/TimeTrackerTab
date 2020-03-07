10b7273a14b4e11de17ab5efe6a54c92fb34b923
tpxcode <tpxcode@tpxcodes-iMac.local>
2020-03-07 06:45:43 -0800

    Initial Commit

---
+++ TimeTrackerTabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTrackerTabs
+//
+//  Created by tpxcode on 07/03/2020.
+//  Copyright © 2020 RenaultRobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
